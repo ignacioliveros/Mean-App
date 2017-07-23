b16effe6364461f82b43fe756bb60433bcfd30cf
ignacioliveros <ignacioliveros@gmail.com>
2017-07-23 00:41:46 -0300

    adedd client route

--- src/server/routes/api/clients/clients.route.ts
+++ src/server/routes/api/clients/clients.route.ts
@@ -1,15 +1,28 @@
 import { NextFunction, Request, Response, Router } from 'express';
+import { ClientModel } from '../../../models/client.model';
+import { ClienRepository, IClienRepository } from '../../../repositories/client-repository/client.repository';
+
 
 export class ClientsRoute {
 
+    private clientRepo: IClienRepository;
+
     constructor(private userRoute: Router) {
         this.routesSet();
+        this.clientRepo = new ClienRepository(ClientModel);
     }
 
     private routesSet() {
         this.userRoute.route('/')
             .get((req: Request, res: Response) => {
-                res.json({ message: 'Hello from clients' });
+                this.clientRepo.GetAll()
+                    .then((data) => {
+                        if (data.err) {
+                            res.status(500).send(data.err);
+                        } else {
+                            res.json(data.entities);
+                        }
+                    });
             });
     }
 }
